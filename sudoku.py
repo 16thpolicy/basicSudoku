cf8f7101af461bb06c8329f2b0497d9093990b1c
16thpolicy <wangz28@rpi.edu>
2018-12-24 06:50:11 -0500

    Update sudoku.py
    Formatted printing of sudoku grid

--- sudoku.py
+++ sudoku.py
@@ -1,19 +1,27 @@
 
 
 def printgraph(longvector):
-    horizontal="-"*37+'\n'
+    topandbottom=" "*15+'|'+" "*11+"|\n"
+    horizontal="   "+"-"*37+'\n'
+    modified_horizontal="-"*43+'\n'
     i=0
-    picture=horizontal
+    k=0
+    picture=topandbottom+horizontal
     for j in longvector:
         if(i==0):
-            picture+='| '+str(j)
             i+=1
+            picture+="   | "+str(j)
         elif(i==8):
             i=0
-            picture=picture+' | '+str(j)+' |\n'+horizontal
+            if(k==2 or k==5):
+                picture+=' | '+str(j)+' |\n'+modified_horizontal
+            else:
+                picture+=' | '+str(j)+' |\n'+horizontal
+            k+=1
         else:
             i+=1
-            picture=picture+' | '+str(j)
+            picture+=' | '+str(j)
+    picture+=topandbottom
     print(picture)
 
 
@@ -24,4 +32,30 @@ if __name__=="__main__":
     longvector[1]=7
     longvector[2]=6
     longvector[3]=9
+    longvector[10]=1
+    longvector[14]=6
+    longvector[19]=4
+    longvector[21]=3
+    longvector[23]=5
+    longvector[24]=8
+    longvector[27]=4
+    longvector[33]=2
+    longvector[34]=1
+    longvector[37]=9
+    longvector[39]=5
+    longvector[46]=5
+    longvector[49]=4
+    longvector[51]=3
+    longvector[53]=6
+    longvector[55]=2
+    longvector[56]=9
+    longvector[62]=8
+    longvector[65]=4
+    longvector[66]=6
+    longvector[67]=9
+    longvector[69]=1
+    longvector[70]=7
+    longvector[71]=3
+    longvector[77]=1
+    longvector[80]=4
     printgraph(longvector)
